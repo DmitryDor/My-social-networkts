9897cb8e0abcabd11090835456646fd7952f95f8
DmitryDor <dmitrydoroshenko80@gmail.com>
2020-07-31 06:57:30 +0300

    37(store)

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,7 +1,3 @@
-let rerenderEntireTree = (state: StateType) => {
-    console.log("state changed")
-}
-
 export type DialogType = {
     id: string,
     name: string
@@ -36,8 +32,87 @@ export type StateType = {
     dialogsPage: DialogsPageType
 }
 
+export type StoreType = {
+    _state: StateType
+    _callSubscriber: (state: StateType) => void
+    addPost: () => void
+    addMessage: () => void
+    updateNewPostText: (newText: string) => void
+    updateNewMessageText: (newText: string) => void
+    subscribe: (observer: (state: StateType) => void) => void
+    getState: ( ) => StateType
 
-export let state = {
+}
+
+
+export let store: StoreType = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: "1", message: "Hi, how are you?", likesCount: 10},
+                {id: "2", message: "It's my post", likesCount: 5},
+                {id: "3", message: "it's you post", likesCount: 7}
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: "1", name: "Dimych"},
+                {id: "2", name: "Andrey"},
+                {id: "3", name: "Sveta"},
+                {id: "4", name: "Sasha"},
+                {id: "5", name: "Victor"},
+                {id: "6", name: "Valera"}
+            ],
+
+            messages: [
+                {id: "1", message: "Hi"},
+                {id: "2", message: "How are you?"},
+                {id: "3", message: "I'm fine"},
+                {id: "4", message: "I'm happy"},
+                {id: "5", message: "Happy new year"}
+            ],
+            newMessageText: ''
+        }
+    },
+    _callSubscriber(state: StateType) {
+        console.log("state changed")
+    },
+    addPost() {
+        let newPost = {id: "4", message: this._state.profilePage.newPostText, likesCount: 0};
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = ''
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText: string) {
+        this._state.profilePage.newPostText = newText
+        this._callSubscriber(this._state);
+    },
+    addMessage() {
+        let newMessage = {id: "6", message: this._state.dialogsPage.newMessageText};
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.newMessageText = ''
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText(newText: string) {
+        this._state.dialogsPage.newMessageText = newText
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer: (state: StateType) => void) {
+        this._callSubscriber = observer
+    },
+    getState(){
+    return this._state
+}
+
+}
+
+/*let rerenderEntireTree = (state: StateType) => {
+    console.log("state changed")
+}*/
+
+
+/*export let state = {
     profilePage: {
         posts: [
             {id: "1", message: "Hi, how are you?", likesCount: 10},
@@ -65,33 +140,34 @@ export let state = {
         ],
         newMessageText: ''
     }
-}
+}*/
 
 
-export const addPost = () => {
+/*export const addPost = () => {
     let newPost = {id: "4", message: state.profilePage.newPostText, likesCount: 0};
     state.profilePage.posts.push(newPost);
     state.profilePage.newPostText = ''
     rerenderEntireTree(state);
-}
+}*/
 
-export const updateNewPostText = (newText: string) => {
+/*export const updateNewPostText = (newText: string) => {
     state.profilePage.newPostText = newText
     rerenderEntireTree(state);
-}
+}*/
 
-export const addMessage = () => {
+/*export const addMessage = () => {
     let newMessage = {id: "6", message: state.dialogsPage.newMessageText};
     state.dialogsPage.messages.push(newMessage);
     state.dialogsPage.newMessageText = ''
     rerenderEntireTree(state);
-}
+}*/
 
-export const updateNewMessageText = (newText: string) => {
+/*export const updateNewMessageText = (newText: string) => {
     state.dialogsPage.newMessageText = newText
     rerenderEntireTree(state);
-}
+}*/
+/*
 
 export const subscribe = (observer: any) => {
     rerenderEntireTree = observer
-}
+}*/
