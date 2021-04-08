86889ad95d3940c828aab594fd3a906ea63b19db
Your NameDmitryDor <you@example.com>
2021-04-08 12:19:27 +0300

    Updates

---
+++ precache-manifest.69aa0c3f58af9016c57d1f269c42b47a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37c9bb2f1e2f5c74890babc05ee7688",
+    "url": "/My-social-networkts62/index.html"
+  },
+  {
+    "revision": "bc40b686a206098b8877",
+    "url": "/My-social-networkts62/static/css/3.6b723291.chunk.css"
+  },
+  {
+    "revision": "e22844f2fbc5341dc0d7",
+    "url": "/My-social-networkts62/static/css/4.1301eca2.chunk.css"
+  },
+  {
+    "revision": "96ea40359d2b57907ab5",
+    "url": "/My-social-networkts62/static/css/main.7faa150e.chunk.css"
+  },
+  {
+    "revision": "b00c47b35b6c70ecc1e2",
+    "url": "/My-social-networkts62/static/js/2.bc32fb67.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/My-social-networkts62/static/js/2.bc32fb67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc40b686a206098b8877",
+    "url": "/My-social-networkts62/static/js/3.21e9eef7.chunk.js"
+  },
+  {
+    "revision": "e22844f2fbc5341dc0d7",
+    "url": "/My-social-networkts62/static/js/4.4e44a48f.chunk.js"
+  },
+  {
+    "revision": "96ea40359d2b57907ab5",
+    "url": "/My-social-networkts62/static/js/main.da43481d.chunk.js"
+  },
+  {
+    "revision": "8e5a2f1a24a9773c8852",
+    "url": "/My-social-networkts62/static/js/runtime-main.e09dea51.js"
+  },
+  {
+    "revision": "f048cdb3f7781dc4689f9a98645d049c",
+    "url": "/My-social-networkts62/static/media/preloader.f048cdb3.webp"
+  }
+]);
